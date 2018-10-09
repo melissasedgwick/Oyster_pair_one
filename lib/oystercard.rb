1c3826deed24f8bab924a8a9f10639252435f5b3
Br0ckers <neil.brockman@yahoo.co.uk>
2018-10-09 14:55:08 +0100

    Added functionalities for touch_in and touch_out methods. Made deduct method private. (Upto step 10)

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,11 @@
 class Oystercard
   MAX_BALANCE = 90
+  MIN_BALANCE = 1
   attr_reader :balance
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(money)
@@ -14,5 +16,21 @@ class Oystercard
   def deduct(money)
     @balance -= money
   end
+
+  def touch_in
+    raise "Error: balance below minimum" if balance < MIN_BALANCE
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+    deduct(MIN_BALANCE)
+  end
+
+  def in_journey?
+    return @in_journey
+  end
+
+  private :deduct
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -24,11 +24,52 @@ describe Oystercard do
     end
   end
 
-  describe '#deduct' do
-    it { is_expected.to respond_to(:deduct).with(1).argument }
+  # describe '#deduct' do
+  #   it { is_expected.to respond_to(:deduct).with(1).argument }
+  #
+  #   it 'Can deduct from balance' do
+  #     expect{ subject.deduct(1) }.to change{ subject.balance}.by -1
+  #   end
+  # end
 
-    it 'Can deduct from balance' do
-      expect{ subject.deduct(1) }.to change{ subject.balance}.by -1
+  describe '#touch_in' do
+    it { is_expected.to respond_to(:touch_in) }
+
+    it 'Raises error if below minimum balance' do
+      expect{subject.touch_in}.to raise_error("Error: balance below minimum")
+    end
+  end
+
+  describe '#touch_out' do
+    it { is_expected.to respond_to(:touch_out) }
+
+    it 'Deduct fare after touching out' do
+      subject.top_up(10)
+      subject.touch_in
+      expect{subject.touch_out}.to change{ subject.balance}.by -Oystercard::MIN_BALANCE
+    end
+
+
+  end
+
+  describe '#in_journey?' do
+    it { is_expected.to respond_to(:in_journey?) }
+
+    it 'returns false when initiated' do
+      expect(subject.in_journey?).to eq(false)
+    end
+
+    it 'returns true after touching in' do
+      subject.top_up 10
+      subject.touch_in
+      expect(subject.in_journey?).to eq(true)
+    end
+
+    it 'returns false after touching out' do
+      subject.top_up 10
+      subject.touch_in
+      subject.touch_out
+      expect(subject.in_journey?).to eq(false)
     end
   end
 
